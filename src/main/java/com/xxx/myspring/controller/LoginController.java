733a872ebf7b1aad6d3fed413ade81fdd53429e5
GitHub账号 <GitHub>
2022-10-09 11:15:36 +0800

    修改controller

--- src/main/java/com/xxx/myspring/controller/LoginController.java
+++ src/main/java/com/xxx/myspring/controller/LoginController.java
@@ -1,4 +1,23 @@
 package com.xxx.myspring.controller;
 
-public class LoginController {
+import com.xxx.myspring.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/login")
+public class LoginController extends HttpServlet {
+    @Autowired
+    private UserService userService;
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        userService.queryByUserId("123");
+        resp.getWriter().write("success");
+    }
 }
